60070872448eb27d1e27524405878871311114f5
Vladimir <vovandrop@gmail.com>
2025-02-07 21:19:55 +0300

    Добавил модуль с звуком

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import './styles.css'
 
-import { BackgroundModule } from './background.module.js';
+import { BackgroundModule } from './modules/background.module';
 const backgroundModule = new BackgroundModule('background', 'Изменить цвет фона');
 backgroundModule.trigger();
+
+import { RandomAudioModule } from './modules/randomAudio';
+const randomAudioModule = new RandomAudioModule('randomAudio', 'Случайный звук');
+randomAudioModule.trigger();
