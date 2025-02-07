49d59c7ca4f3dc3308e6cf363e8e04fe87460a6c
Vladimir <vovandrop@gmail.com>
2025-02-07 20:53:02 +0300

    Добавил модуль с фоном

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 import './styles.css'
 
+import { BackgroundModule } from './background.module.js';
+const backgroundModule = new BackgroundModule('background', 'Изменить цвет фона');
+backgroundModule.trigger();
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,28 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
+    constructor (type, text) {
+            super(type, text);
+            this.type = type;
+            this.text = text;
+        
+    }
+
+    trigger() {
+    const button = document.createElement('button');
+    button.textContent = 'Изменить цвет фона';
+
+    function getRandomHexColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    }
+
+    button.addEventListener('click', function() {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+    });
+
+
+    document.body.appendChild(button);
+}
 
 }
